c68c8a8ed9136157cf7f98bd77481dd8b8c44eba
h.yoshida <hiroki.yoshida01@outlook.jp>
2021-08-21 16:27:33 +0900

    Revert "add index"
    This reverts commit 48ecc4e7

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -26,7 +26,6 @@ CREATE TABLE `isu_condition` (
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
-alter table `isu_condition` add index idx1 (timestamp);
 
 CREATE TABLE `user` (
   `jia_user_id` VARCHAR(255) PRIMARY KEY,
